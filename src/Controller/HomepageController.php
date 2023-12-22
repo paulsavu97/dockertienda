c69e65bae554721d89b79704ee3e5d6e6204ffea
paulsavu97 <paul.savu@pcmancha.com>
2023-12-22 02:00:40 +0100

    correccion

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,19 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
-    public function indexx(ProductoRepository $productoRepository): Response
-    {
-        $productos[] = $productoRepository->findAll_limit8();
-        $productos[] = $productoRepository->findProductsByNovelty();
-        $productos[] = $productoRepository->findProductsByDiscount();
-        $productos[] = $productoRepository->findTopSellingProducts();
-
-        return $this->render('/index.html.twig', [
-            'productos' => $productos,
-        ]);
-    }
-
-    #[Route('/homepage/{_locale}', name: 'app_homepage_index', methods: ['GET'], requirements: ['_locale' => 'en|es'])]
     public function index(ProductoRepository $productoRepository): Response
     {
         $productos[] = $productoRepository->findAll_limit8();
@@ -35,6 +22,19 @@ class HomepageController extends AbstractController
             'productos' => $productos,
         ]);
     }
+
+   /*  #[Route('/homepage/{_locale}', name: 'app_homepage_index', methods: ['GET'], requirements: ['_locale' => 'en|es'])]
+    public function index(ProductoRepository $productoRepository): Response
+    {
+        $productos[] = $productoRepository->findAll_limit8();
+        $productos[] = $productoRepository->findProductsByNovelty();
+        $productos[] = $productoRepository->findProductsByDiscount();
+        $productos[] = $productoRepository->findTopSellingProducts();
+
+        return $this->render('/index.html.twig', [
+            'productos' => $productos,
+        ]);
+    } */
 
 
     #[Route('/contacto/{_locale}', name: 'app_contacto_panel', methods: ['GET'], requirements: ['_locale' => 'en|es'])]
